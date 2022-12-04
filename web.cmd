f59b1666e6c1dd862845ad64f31910147cdd6851
Vicent Bellés <vicentbelles@gmail.com>
2022-12-04 17:09:37 +0100

    Updated web.cmd

--- web.cmd
+++ web.cmd
@@ -1,6 +1,6 @@
 @echo on
 mkdir build-web
 cd build-web
-emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"  && ^
+emcmake cmake -G "MinGW Makefiles" .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"  && ^
 emmake make && ^
 python -m http.server 8000
